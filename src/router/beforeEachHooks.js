2fc1d65f543606981c7c9fe316498a2a6b0f890e
Goldwin Japar <goldwin.japar@gdn-commerce.com>
2019-08-14 15:47:46 +0700

    pretify code eslint for router

--- src/router/beforeEachHooks.js
+++ src/router/beforeEachHooks.js
@@ -1,22 +1,21 @@
-import Vue from 'vue'
 import { $auth } from '@helper'
 
 export default {
   // Check the login status
-  checkLoginAuth (to, from, next) {
+  checkLoginAuth(to, from, next) {
     if (to.meta && to.meta.ignoreAuth) {
       next()
     } else {
       if ($auth.checkSession()) {
         next()
       } else {
-        next({ path: '/login'})
+        next({ path: '/login' })
       }
     }
   },
 
   // Check page permissions
-  checkPageAuth (to, from, next) {
+  checkPageAuth(to, from, next) {
     if (to.meta && to.meta.ignoreAuth) {
       next()
     } else {
